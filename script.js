d3916d287e804c1b4f1d9d5abfe21d444dc6580e
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2022-03-04 18:40:49 -0300

    creo que ya esta

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-// console.log(parseInt(Math.random()*3+1))
-
 alert("¡Bienvenido al juego!");
 
 function comenzar() {
 
     let confirmacion = confirm("¿Desea iniciar un nuevo juego?");
+    let elegir;
+    let maquina = parseInt(Math.random()*3+1);
+
 
     if (confirmacion){
-        prompt("Por favor, ingresar nombre de jugador")
+        let nombre = prompt("Por favor, ingresar nombre de jugador");
+        nombre
+
+        elegir = parseInt(prompt("Elegir un número"));
+
+
+        if (maquina === 3 && elegir === 2){
+            alert("El ganador es: la maquina :(")
+        } else if (maquina === 2 && elegir === 1){
+            alert("El ganador es: la maquina :(") 
+        } else if (maquina === 1 && elegir === 3){
+            alert("El ganador es: la maquina :(")
+        } else if (maquina === elegir){
+            alert("El ganador es: ¡no hay! Empate -_-")
+        } else alert("El ganador es: ¡" + nombre + "! :)")
+
     }
 
 }
