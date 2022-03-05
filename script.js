1edf47058c8e0293521672c50b945843c79462b5
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2022-03-05 19:41:44 -0300

    arreglado round y nombre vacio

--- script.js
+++ script.js
@@ -8,29 +8,34 @@ function comenzar() {
 
     if (confirmacion){
         let nombre = prompt("Por favor, ingresar nombre de jugador");
-        
-        let contadorUsuario = 0;
-        let contadorPC= 0;
+    
+        if (nombre){
+            alert("¡Debe escribir un nombre!");    
+        } else {
+            let contadorUsuario = 0;
+            let contadorPC= 0;
 
-        for (let i = 0; i < 3; i++) {
-            elegir = parseInt(prompt("Elegir un número"));
-            let resultado = ronda(elegir);
+            for (let i = 0; i < 3; i++) {
+                elegir = parseInt(prompt("Elegir un número"));
+                let resultado = ronda(elegir);
 
-            if (resultado === 0){
-                contadorPC++;
-                alert("Ganador de ronda PC");
-            } else if (resultado === 1){
-                contadorUsuario++;
-                alert("Ganador de ronda " + nombre);
-            } else alert("Empate de ronda");
-            
+                if (resultado === 0){
+                    contadorPC++;
+                    alert("Ganador de ronda PC");
+                } else if (resultado === 1){
+                    contadorUsuario++;
+                    alert("Ganador de ronda " + nombre);
+                }else if (resultado === 2){
+                    i--;
+                } else alert("Empate de ronda");
+            }
+
+            if (contadorPC > contadorUsuario){
+                alert("Ganador de la partida PC");
+            } else if (contadorPC < contadorUsuario){
+                alert("Ganador de la partida " + nombre);
+            } else alert("Empate de partida");
         }
-
-        if (contadorPC > contadorUsuario){
-            alert("Ganador de la partida PC");
-        } else if (contadorPC < contadorUsuario){
-            alert("Ganador de la partida " + nombre);
-        } else alert("Empate de partida");
     }
 }
 
@@ -39,16 +44,19 @@ function ronda (elegir){
     let maquina = parseInt(Math.random()*3+1);
     
     if (elegir >= 1 && elegir <= 3){
-    if (maquina === 3 && elegir === 2){
-        return 0;
-    } else if (maquina === 2 && elegir === 1){
-        return 0;
-    } else if (maquina === 1 && elegir === 3){
-        return 0;
-    } else if (maquina === elegir){
-        return -1;
+        if (maquina === 3 && elegir === 2){
+            return 0;
+        } else if (maquina === 2 && elegir === 1){
+            return 0;
+        } else if (maquina === 1 && elegir === 3){
+            return 0;
+        } else if (maquina === elegir){
+            return -1;
+        } else {
+            return 1;
+            }
     } else {
-        return 1;
+        alert("ELEGIR ENTRE 1 Y 3");
+        return 2;
         }
-    } else alert("ELEGIR ENTRE 1 Y 3");
 }
